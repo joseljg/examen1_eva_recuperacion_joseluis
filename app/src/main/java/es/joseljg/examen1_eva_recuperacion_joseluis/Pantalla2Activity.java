da1082846d763af939506f1cfe60684aa9c8f060
joseljg <joseljg@gmail.com>
2023-03-08 20:57:38 +0100

    version 0

---
+++ app/src/main/java/es/joseljg/examen1_eva_recuperacion_joseluis/Pantalla2Activity.java
@@ -0,0 +1,14 @@
+package es.joseljg.examen1_eva_recuperacion_joseluis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+    }
+}
